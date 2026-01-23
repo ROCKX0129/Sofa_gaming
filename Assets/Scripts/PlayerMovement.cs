38ba7f273d4f653581cf416bb75cb258e7bcab01
ZenoVerse090 <husnihassan7@gmail.com>
2026-01-23 18:08:17 +0200

    fixed movement
    I Fixed movement issues and now local multiplayer works but only one person can be on the keyboard, and the other has to use a controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,33 +1,56 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    Vector2 moveInput;
-    public float speed;
+    private PlayerInput input;
+    private Rigidbody2D rb;
+
+    private Vector2 moveInput;
+    private bool jumpPressed;
+
+    // Movement
+    public float moveSpeed;
     public float jumpForce;
 
-    Rigidbody2D rb;
+    // Ground Check
+    public Transform groundCheck;
+    public float groundradius;
+    public LayerMask groundLayer;
+
+    private bool isGrounded;    
+
     void Awake()
     {
+        input = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     public void OnMove(InputAction.CallbackContext ctx)
     {
         moveInput = ctx.ReadValue<Vector2>();
-        Debug.Log("Move" + moveInput);
+        Debug.Log(gameObject.name + " MOVE: " + moveInput);
     }
 
     public void OnJump(InputAction.CallbackContext ctx)
     {
-        if (!ctx.performed) return;
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        if (ctx.performed)
+            jumpPressed = true;
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(moveInput.x * speed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundradius, groundLayer);
+
+        if (jumpPressed && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+        else
+        {
+            jumpPressed = false;
+        }
     }
 }
