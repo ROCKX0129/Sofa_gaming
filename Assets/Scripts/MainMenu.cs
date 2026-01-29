31ab256e719b66279dad7d1664db484b7aae2722
Renn <its.happiness.2005@gmail.com>
2026-01-29 17:08:07 +0200

    menu ui is added again :(
    the floor and gameobjects from level was visible so they'll now be deactivated once playmode starts

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,27 +3,43 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-   
     public GameObject mainMenuPanel;
+    // settings panel is for now deactivated i'll work on it next week
     public GameObject settingsPanel;
 
-    public string mainScene = "MainScene";
+    public string levelSceneName = "level";
 
-    private void Start()
+    private void Awake()
     {
+        CleanupOtherScenes();
         ShowMainMenu();
     }
 
+    // Removing any other loaded scenes so the floor and other stuff doesnt appear
+    private void CleanupOtherScenes()
+    {
+        Scene activeScene = SceneManager.GetActiveScene();
+
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene scene = SceneManager.GetSceneAt(i);
+
+            if (scene != activeScene)
+            {
+                SceneManager.UnloadSceneAsync(scene);
+            }
+        }
+    }
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(mainScene);
+       
+        SceneManager.LoadScene(levelSceneName, LoadSceneMode.Single);
     }
 
     public void ExitGame()
     {
-            Application.Quit();
-       
+        Application.Quit();
     }
 
     public void OpenSettings()
@@ -36,7 +52,6 @@ public class MenuManager : MonoBehaviour
     {
         ShowMainMenu();
     }
-
 
     private void ShowMainMenu()
     {
