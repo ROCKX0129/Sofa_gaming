19ae7ce7d25e250b4770ea689795b27242d575b0
ZenoVerse090 <husnihassan7@gmail.com>
2026-02-02 00:04:36 +0200

    Combat done
    Basic combat is done, all there is needed is the animations.

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -67,7 +67,6 @@ public class Combat : MonoBehaviour
     void OnDrawGizmosSelected()
     {
         if (attackPoint == null) return;
-
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(attackPoint.position, attackSize);
     }
