62ad62d0fc713601c1503f54c061b76cf547f924
Enro Rockx <q2210029101@gmail.com>
2026-02-06 07:57:23 +0200

    animation binding, and also some codo change for PlayerMoving, add new animation binding script

---
+++ Assets/Animation/Animation_Scripts/Animation_connecting_scripts.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Animation_connecting_scripts : MonoBehaviour
+{
+    public GameObject Character_prefab;
+    private Animator mAnimator;
+    private bool ismoving;
+    private PlayerController playerController;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+
+        mAnimator = Character_prefab.GetComponent<Animator>();
+        Debug.Log(mAnimator);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerController.moveInput != null)
+        {
+            if (playerController.moveInput.x != 0.0f)
+            {
+                ismoving = true;
+            }
+            else
+            {
+                ismoving = false;
+            }
+
+
+            mAnimator.SetBool("OnMoving", ismoving);
+
+        }
+    }
+}
